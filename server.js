416aa07061a036c02dc0446ee8e2a7baa4749d6c
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-04-04 12:07:17 -0700

    new deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8080;
+const PORT = process.env.PORT ||8080;
 
 const app = express();
 
@@ -15,14 +15,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false});
+
+mongoose.connection.on("error", (err) =>
+  console.log(`error in mongoose connection: ${err.message}`)
+);
+
+mongoose.connection.once("open", () => {
+  console.log("mongoose connected!");
+  app.listen(PORT, (err) => console.log(`http://localhost/${PORT}`));
 });
+
+
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
