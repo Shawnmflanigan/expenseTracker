d9006648552022b78288740765ec9d7ce1b5189a
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-04-05 13:32:43 -0700

    fix service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -48,13 +48,45 @@ const DATA_CACHE_NAME = "data-cache-v1";
     self.clients.claim();
   });
 
-  self.addEventListener('fetch', function(evt) {
-    // code to handle requests goes here
+  // self.addEventListener('fetch', function(evt) {
+  //   // code to handle requests goes here
+  //   evt.respondWith(
+  //       caches.open(CACHE_NAME).then(cache => {
+  //         return cache.match(evt.request).then(response => {
+  //           return response || fetch(evt.request);
+  //         });
+  //       })
+  //     );
+  //   });
+  // fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-          return cache.match(evt.request).then(response => {
-            return response || fetch(evt.request);
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
           });
-        })
-      );
-    });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
