df854f230f444b1fbabb8709958e70e9fc4ed588
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-04-05 10:43:09 -0700

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT ||8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -26,7 +26,7 @@ mongoose.connection.on("error", (err) =>
 
 mongoose.connection.once("open", () => {
   console.log("mongoose connected!");
-  app.listen(PORT, (err) => console.log(`http://localhost/${PORT}`));
+  app.listen(PORT, (err) => console.log(`http://localhost:${PORT}`));
 });
 
 
@@ -34,6 +34,3 @@ mongoose.connection.once("open", () => {
 // routes
 app.use(require("./routes/api.js"));
 
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
